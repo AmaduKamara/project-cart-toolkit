5ada3eb50282afdd1e8703b26fdd33214f2e43fc
Amkam <amaduskamara@gmail.com>
2022-01-30 08:21:53 +0000

    Add ui slice, access store from CartButton component and conditionally render cart in App component

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,17 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
 
-const CartButton = (props) => {
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+
+const CartButton = () => {
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
