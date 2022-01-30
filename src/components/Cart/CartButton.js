e19a06ddd16f0a19fa6e7b1ac069810d68d4c7c5
Amkam <amaduskamara@gmail.com>
2022-01-30 09:15:03 +0000

    Display cartQuantity fetched from stone in CartButton"

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import classes from "./CartButton.module.css";
 import { uiActions } from "../../store/ui-slice";
 
 const CartButton = () => {
   const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
 
   const toggleCartHandler = () => {
     dispatch(uiActions.toggle());
@@ -13,7 +14,7 @@ const CartButton = () => {
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
