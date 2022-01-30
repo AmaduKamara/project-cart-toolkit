97e2eeae03aaa62e0e0ba3ebdd7fd80d2587114c
Amkam <amaduskamara@gmail.com>
2022-01-30 09:05:36 +0000

    Work on adding items to cart in product item component

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,24 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import { useDispatch } from "react-redux";
+import cartActions from "../../store/cart-slice";
+
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const { id, title, price, description } = props;
+
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+        description,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -13,8 +29,8 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
-        </div>
+          <button onClick={addToCartHandler}>Add to Cart</button>
+        </div> 
       </Card>
     </li>
   );
