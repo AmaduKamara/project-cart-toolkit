43a019c89acb249aacb526338db636e03b4c8780
Amkam <amaduskamara@gmail.com>
2022-01-30 08:50:57 +0000

    Add removeItemFromCart reducer function

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -23,6 +23,15 @@ const cartSlice = createSlice({
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if ((existingItem.quantity = 1)) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
